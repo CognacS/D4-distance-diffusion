78e6faeb13108234914a62bfd27844d5f0e8694d
CognacS <samuel.cognolato@gmail.com>
2025-09-24 20:22:47 +0000

    added dataset downloader and configs for hparams search

---
+++ download_dataset.py
@@ -0,0 +1,89 @@
+# suppress warnings from torch 2.4.1, these will be fixed in future versions
+import warnings
+warnings.filterwarnings("ignore", "You are using `torch\.load` with `weights_only=False`.*")
+warnings.filterwarnings("ignore", ".*deterministic implementation.*")
+warnings.filterwarnings("ignore", "Weights only load failed\. Please file an issue to make `torch\.load\(weights_only=True\)`.*")
+warnings.filterwarnings("ignore", "The `pre_transform` argument differs from the one used in the pre-processed version of this dataset\..*")
+warnings.simplefilter("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=RuntimeWarning, message=".*to-Python converter for.*")
+
+import hydra
+from omegaconf import DictConfig, OmegaConf, open_dict
+from hydra.core.hydra_config import HydraConfig
+
+import src.data.datasets as datasets
+from src.data.transforms import reg_transforms
+from src.data.filters import reg_filters
+from src.datatypes import reg_dataset_wrapper
+
+from copy import deepcopy
+
+# import monkey patches for fixing bugs in torch and torch_geometric for MPS devices
+import platform
+if 'darwin' in platform.system().lower(): # if running on MacOS, apply monkey patches
+    import src.monkey_patches
+
+@hydra.main(version_base=None, config_path='config', config_name='default')
+def main(cfg: DictConfig):
+
+    # make hydra config available in the current config
+    # will be removed later
+    OmegaConf.set_struct(cfg, True)
+    with open_dict(cfg):
+        cfg.hydra = HydraConfig.get()
+
+
+    cfg_dataset = cfg.task.dataset
+    cfg_pretf = cfg.task.pre_transform
+
+    ###########################  DATASET SETUP  ############################
+    # get dataset name
+    dataset_name = cfg_dataset.name
+    dataset_params = cfg_dataset.params
+
+    print(f'Downloading dataset "{dataset_name}"')
+
+    pre_transform = None
+    if isinstance(cfg_pretf, list):
+        pre_transform = [reg_transforms.get_instance_from_dict(tf) for tf in cfg_pretf]
+    else:
+        pre_transform = reg_transforms.get_instance_from_dict(cfg_pretf)
+
+    
+    # add filters if any
+    addons = {k : None for k in ['pre_filter', 'pre_filter_raw']}
+    dataset_params_mod = deepcopy(dataset_params)
+    for k in addons.keys():
+        if k in dataset_params_mod:
+            if dataset_params_mod[k] is not None:
+                cfg_prefilt = dataset_params_mod[k]
+                if isinstance(cfg_pretf, list):
+                    v = [reg_filters.get_instance_from_dict(tf) for tf in cfg_prefilt]
+                else:
+                    v = reg_filters.get_instance_from_dict(cfg_prefilt)
+                addons[k] = v
+                dataset_params_mod.pop(k, None)
+
+    # get dataset resources, also include pre_transform
+    data_resources = datasets.reg_dataresources.get_instance(
+        dataset_name, dataset_params_mod, pre_transform=pre_transform, **addons
+    )
+
+    # add wrappers to dataset if there are any
+    if cfg_dswrapper is not None:
+        if not isinstance(cfg_dswrapper, list):
+            cfg_dswrapper = [cfg_dswrapper]
+        dataset_wrappers = [(reg_dataset_wrapper.get_class(dsw['name']), dsw['params']) for dsw in cfg_dswrapper]
+        
+        # dataset wrappers will wrap the datasets returned by the data resource
+        # a wrapper can add new data to the dataset elements and info
+        # TODO: make wrappers save data, avoiding to recompute it every time a new dataset instance is created
+        data_resources.add_dataset_wrapper(dataset_wrappers)
+        
+    data_resources.prepare_data()
+        
+    print(f'Dataset "{dataset_name}" is ready.')
+
+
+if __name__ == '__main__':
+    main()
